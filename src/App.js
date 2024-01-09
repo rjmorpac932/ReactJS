7a6128f252b52ba229ad76ef07a67c37e9037245
Tibu <tibumayo88@gmail.com>
2024-01-09 20:26:16 +0100

    Subida Componente Lista Tibu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './ComponentesP1/Navbar';
-import Formulario from './ComponentesP1/Formulario';
-import Footer from './ComponentesP1/Footer';
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./ComponentesP1/Navbar";
+import Formulario from "./ComponentesP1/Formulario";
+import Footer from "./ComponentesP1/Footer";
+import Lista from "./ComponentesP1/Lista";
 
 function App() {
   return (
     <div className="App">
       <strong>Componente José Antonio</strong>
-      <NavBar/>
+      <NavBar />
       <strong>Componente Alicia</strong>
       <Formulario></Formulario>
       <strong>Componente Carmen</strong>
       {/* tu componente aquí */}
       <strong>Componente Tibu</strong>
-      {/* tu componente aquí */}
+      <Lista />
       <strong>Componente Adrián</strong>
       {/* tu componente aquí */}
       <strong>Componente Pablo</strong>
-      <Footer ></Footer>
+      <Footer></Footer>
     </div>
   );
 }
