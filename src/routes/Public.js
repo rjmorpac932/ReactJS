c2b8cfeee6db34e0f4b7e3bf93841a7541c6316c
alopvaz <alopvaz014@iesalmudeyne.es>
2024-01-21 15:18:44 +0100

    Cambios alicia practica 2

--- src/routes/Public.js
+++ src/routes/Public.js
@@ -4,6 +4,7 @@ import NotFound from "../pages/NotFound";
 import Menu from "../components/Menu";
 import RutasAnidadas from "../pages/RutasAnidadas";
 import FormularioNotas from "../ComponentesP2/FormularioNotas";
+import Actualizacion from "../ComponentesP2/Actualizacion";
 import NavBar from "../ComponentesP1/Navbar";
 import Formulario from "../ComponentesP1/Formulario";
 import Boton from "../ComponentesP1/Boton";
@@ -29,7 +30,7 @@ function Public() {
                         <Route path="tibu" element={<Lista/>}/>   
                     </Route>
                     <Route path="/practica2/" element={<RutasAnidadas />} >
-                        <Route path="alicia" element={<p>Formulario con evento de Alicia</p>}/>
+                        <Route path="alicia" element={<Actualizacion/>}/>
                         <Route path="adrian" element={<p>Formulario con evento de Adrián</p>}/>
                         <Route path="carmen" element={<p>Formulario con evento de Carmen</p>}/>
                         <Route path="joseantonio" element={<FormularioNotas></FormularioNotas>}/>
