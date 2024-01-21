a25d51a08f1ddf18674c10b1ec7a72c2100d7347
tmaygon609@iesalmudeyne.es <tmaygon609@iesalmudeyne.es>
2024-01-21 13:28:35 +0100

    Intoduccion formulario registro en el public.js

--- src/routes/Public.js
+++ src/routes/Public.js
@@ -10,9 +10,10 @@ import Boton from "../ComponentesP1/Boton";
 import CambioColorEnMouse from "../ComponentesP1/OnMouseOver";
 import Footer from "../ComponentesP1/Footer";
 import Lista from "../ComponentesP1/Lista";
+import RegistrarUsuarioForm from "../ComponentesP2/registrarUsuario";
 
 function Public() {
-    return ( 
+    return (
         <div>
             <h1>Prácticas React</h1>
             <Router>
@@ -21,25 +22,25 @@ function Public() {
                     <Route path="/" element={<p></p>} />
                     <Route path="*" element={<NotFound />} />
                     <Route path="/practica1/" element={<RutasAnidadas />} >
-                        <Route path="alicia" element={<Formulario></Formulario>}/>
-                        <Route path="adrian" element={<Boton/>}/>
-                        <Route path="carmen" element={<CambioColorEnMouse></CambioColorEnMouse>}/>
-                        <Route path="joseantonio" element={<NavBar/>}/>
-                        <Route path="pablo" element={<Footer></Footer>}/>
-                        <Route path="tibu" element={<Lista/>}/>   
+                        <Route path="alicia" element={<Formulario></Formulario>} />
+                        <Route path="adrian" element={<Boton />} />
+                        <Route path="carmen" element={<CambioColorEnMouse></CambioColorEnMouse>} />
+                        <Route path="joseantonio" element={<NavBar />} />
+                        <Route path="pablo" element={<Footer></Footer>} />
+                        <Route path="tibu" element={<Lista />} />
                     </Route>
                     <Route path="/practica2/" element={<RutasAnidadas />} >
-                        <Route path="alicia" element={<p>Formulario con evento de Alicia</p>}/>
-                        <Route path="adrian" element={<p>Formulario con evento de Adrián</p>}/>
-                        <Route path="carmen" element={<p>Formulario con evento de Carmen</p>}/>
-                        <Route path="joseantonio" element={<FormularioNotas></FormularioNotas>}/>
-                        <Route path="pablo" element={<p>Formulario con evento de Pablo</p>}/>
-                        <Route path="tibu" element={<p>Formulario con evento de Tibu</p>}/>   
+                        <Route path="alicia" element={<p>Formulario con evento de Alicia</p>} />
+                        <Route path="adrian" element={<p>Formulario con evento de Adrián</p>} />
+                        <Route path="carmen" element={<p>Formulario con evento de Carmen</p>} />
+                        <Route path="joseantonio" element={<FormularioNotas></FormularioNotas>} />
+                        <Route path="pablo" element={<p>Formulario con evento de Pablo</p>} />
+                        <Route path="tibu" element={<RegistrarUsuarioForm> </RegistrarUsuarioForm>} />
                     </Route>
                 </Routes>
             </Router>
         </div>
-     );
+    );
 }
 
 export default Public;
