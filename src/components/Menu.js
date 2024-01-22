b2d35e1cff3f4f9207b40f44e4c17964f2fe24f1
carmensanchez19 <msanmar508@iesalmudeyne.es>
2024-01-22 12:00:08 +0100

    Subida corregido

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,8 +5,8 @@ function Menu() {
     return ( 
         <nav>
             <ul style={{ display: "flex", listStyle: "none" }}>
-                <li style={{ marginRight: "10px" }}><NavLink to="/practica1">Spring1</NavLink></li>
-                <li style={{ marginRight: "10px" }}><NavLink to="/practica2">Spring2</NavLink></li>
+                <li style={{ marginRight: "10px" }}><NavLink to="/practica1">Sprint 1</NavLink></li>
+                <li style={{ marginRight: "10px" }}><NavLink to="/practica2">Sprint 2</NavLink></li>
             </ul>
         </nav>
      );
